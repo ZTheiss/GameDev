985a61004e76225a835dbd5e20a927f1ebfe14b5
ZTheiss <ztheiss97@gmail.com>
2022-08-19 22:40:20 -0500

    Added CameraReset

--- Project Escape/PreAlpha/UnityFile/ProjectEscape/Assets/Scripts/CameraSystem.cs
+++ Project Escape/PreAlpha/UnityFile/ProjectEscape/Assets/Scripts/CameraSystem.cs
@@ -21,15 +21,28 @@ namespace camSystem.CameraSystem {
         private Vector2 lastMousePosition;
         private float targetFieldOfView = 50;
         private Vector3 followOffset;
+        private Vector3 originalPosition;
+        private Quaternion originalRotation;
+        private Vector3 originalAngle;
+
 
 
         private void Awake() {
             followOffset = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
+            originalPosition = transform.position;
+            originalRotation = transform.rotation;
+            originalAngle = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset;
         }
 
         private void Update() {
             HandleCameraMovement();
-
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.localRotation = originalRotation;
+                transform.position = originalPosition;
+                followOffset.y = 60;
+                cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>().m_FollowOffset = originalAngle;
+            }
             if (useEdgeScrolling) {
                 HandleCameraMovementEdgeScrolling();
             }
@@ -52,6 +65,7 @@ namespace camSystem.CameraSystem {
             if (Input.GetKey(KeyCode.S)) inputDir.z = -1f;
             if (Input.GetKey(KeyCode.A)) inputDir.x = -1f;
             if (Input.GetKey(KeyCode.D)) inputDir.x = +1f;
+            
 
             Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
 
@@ -114,6 +128,7 @@ namespace camSystem.CameraSystem {
             float rotateDir = 0f;
             if (Input.GetKey(KeyCode.Q)) rotateDir = +1f;
             if (Input.GetKey(KeyCode.E)) rotateDir = -1f;
+
 
             float rotateSpeed = 100f;
             transform.eulerAngles += new Vector3(0, rotateDir * rotateSpeed * Time.deltaTime, 0);
